ff725aa67c51eee5a5100e08033c8f1750a05d19
smriti12397 <78731697+smriti12397@users.noreply.github.com>
2021-10-25 00:10:56 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 // C++ program for Merge Sort
 //Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands.
+// complexcity: Average complexity: n*log(n) //
 #include <iostream>
 using namespace std;
 
